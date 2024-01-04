b823da4638c300c58e2b642dde45044c07e03fb8
Aamish Irfan <aamishirfan2662@gmail.com>
2024-01-04 22:19:14 +0500

    added basic error page

---
+++ error.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps({
+  error: Object
+})
+
+const handleError = () => clearError({ redirect: '/todos' })
+</script>
+
+<template>
+  <div class="error-page">
+    <div>
+      <h2>{{ error.statusCode }}</h2>
+      <Button @click="handleError" class="btn-secondary" label="Go to TODOs" />
+    </div>
+  </div>
+</template>
